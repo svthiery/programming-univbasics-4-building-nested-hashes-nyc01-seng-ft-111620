059bb7fe1cb9ac2adce06d5847a6341bb10bc3d4
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-28 16:26:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,10 @@ def monopoly_with_third_tier
   hash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {:one_piece_owned => 25},
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50
+      },
       :names => {}
     }
   }
